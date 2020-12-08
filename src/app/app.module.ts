6f6b58966ad8a69dbba33a5be278bdf64ec1c59f
Khaled ABBACI <abbacikhaled@gmail.com>
2020-12-08 21:52:25 +0100

    adminlte commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './shared/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
